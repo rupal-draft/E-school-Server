d8cc4c2aea9831262a15b39bcf0a88ab954df199
rupal-draft <rupalpaulgspr@gmail.com>
2024-03-02 00:31:37 +0530

    modified:   .gitIgnore 	modified:   package-lock.json 	modified:   package.json 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import express from "express";
-export const app = express();
+import cors from "cors";
+import mongoose from "mongoose";
+import "dotenv/config";
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+mongoose.connect(process.env.DATABASE).then(() => {
+  console.log("Database Connected Succesfully");
+});
 
 const port = 3000;
 
-app.listen(port, ()=>{
-    console.log(`⚙️   Server is running at port : ${port}`);  
-  })
-
-  app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+app.listen(port, () => {
+  console.log(`Server is running at port : ${port}`);
+});
