3dbe93512a60a07bf64604675bc2813c3787a404
rupal-draft <rupalpaulgspr@gmail.com>
2024-03-07 16:55:53 +0530

    Protected Routes

--- Routes/auth.js
+++ Routes/auth.js
@@ -2,9 +2,12 @@ import express from "express";
 
 const router = express.Router();
 
-import { register, login } from "./../Controllers/auth.js";
+import { register, login, logout, currentUser } from "./../Controllers/auth.js";
+import { requireSignin } from "../Middleware/verify.js";
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/logout", logout);
+router.get("/current-user", requireSignin, currentUser);
 
 export default router;
